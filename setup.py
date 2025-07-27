67604e908cd4e2af8a517e259794a6e61e90c174
prajwal <prajwalhawaldar2@gmail.com>
2025-07-27 20:28:30 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Prajwal",
+    author_email="prajwalhawaldar2@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
